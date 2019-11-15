9630946282db450b07ac11d9024e970076a095e2
Nick Machnik <nick.machnik@gmail.com>
2019-11-15 13:58:07 +0100

    sbatch script

---
+++ submit_blast.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+#
+#-------------------------------------------------------------
+#example script for running a single-CPU serial job via SLURM
+#-------------------------------------------------------------
+#
+#SBATCH --job-name=PROT_blast
+#SBATCH --output=logs/PROT.log
+#
+#Number of cores
+#SBATCH -c 24
+#
+#Define the number of hours the job should run. 
+#Maximum runtime is limited to 10 days, ie. 240 hours
+#SBATCH --time=2:00:00
+#
+#Define the amount of RAM used by your job in GigaBytes
+#SBATCH --mem16Gb
+#
+####SBATCH --partition=bigtb
+#SBATCH --partition=bigmem
+#
+#Send emails when a job starts, it is finished or it exits
+#SBATCH --mail-user=NOPE
+#SBATCH --mail-type=ALL
+#
+#Pick whether you prefer requeue or not. If you use the --requeue
+#option, the requeued job script will start from the beginning, 
+#potentially overwriting your previous progress, so be careful.
+#For some people the --requeue option might be desired if their
+#application will continue from the last state.
+#Do not requeue the job in the case it fails.
+#SBATCH --no-requeue
+#
+#Do not export the local environment to the compute nodes
+#SBATCH --export=NONE
+unset SLURM_EXPORT_ENV
+#
+#
+#load the respective software module you intend to use
+#module load python/3.6.6
+module load ncbi-blast
+#
+#
+#run the respective binary through SLURM's srun
+srun --cpu_bind=verbose CMD
