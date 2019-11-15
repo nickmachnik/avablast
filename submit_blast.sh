5494e06850264248878c59366364ccd292ff356c
Nick Machnik <nick.machnik@gmail.com>
2019-11-15 14:21:31 +0100

    reformat sed

--- submit_blast.sh
+++ submit_blast.sh
@@ -5,7 +5,7 @@
 #-------------------------------------------------------------
 #
 #SBATCH --job-name=PROT_blast
-#SBATCH --output=log/PROT.log
+#SBATCH --output=log/PROT
 #
 #Number of cores
 #SBATCH -c 24
