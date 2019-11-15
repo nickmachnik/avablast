0ed91efc42f7b909cd50c2e419012f498894a0db
Nick Machnik <nick.machnik@gmail.com>
2019-11-15 10:21:16 +0100

    Initial commit

---
+++ all_vs_all_blast.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+DB=$1
+
+echo ${DB}
---
+++ single_fasta_acc.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+import gzip
+import sys
+
+def main():
+    """
+    Return the next fasta entry given a bytes index.
+    """
+    assert len(sys.argv) == 3, """
+        Usage: all_vs_all_blastp.py <path to fasta file> <last index>
+    """
+    header, seq, position = get_next_fasta_entry(sys.argv[1], int(sys.argv[2]))
+    print(header, seq, position)
+    with open('neg_seqids', 'a') as fout:
+        fout.write('>' + header + '\n')
+    with open('curr.fasta', 'w') as fout:
+        fout.write('>' + header + '\n')
+        fout.write(seq + '\n')
+
+    
+
+
+def get_next_fasta_entry(path, start=0):
+    if path.endswith('gz'):
+        o = gzip.open
+        mode = 'rt'
+    else:
+        o = open
+        mode = 'r'
+    with o(path, mode) as fin:
+        seq = ""
+        header = None
+        fin.seek(start)
+        line = fin.readline()
+        last_index = 0
+        while True:
+            if line == '':
+                return (header, seq, 0)
+            elif header != None and line.startswith('>'):
+                return (header, seq, last_index)
+            elif line.startswith('>'):
+                header = line.strip()[1:]
+            else:
+                seq += line.strip()
+            last_index = fin.tell()
+            line = fin.readline()
+
+
+if __name__ == '__main__':
+    main()
