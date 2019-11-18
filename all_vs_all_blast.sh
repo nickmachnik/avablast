2f9ba255d56b72e334b007f66db29478923750be
Nick Machnik <nick.machnik@gmail.com>
2019-11-18 12:55:22 +0100

    split fasta into bigger chunks. do not use -negidseqs

--- all_vs_all_blast.sh
+++ all_vs_all_blast.sh
@@ -2,12 +2,13 @@
 
 DB=$1
 FASTA=$2
+N_JOBS=$3
 
 if [ $# -eq 0 ]
   then
     echo "No arguments supplied.
     Usage:
-        all_vs_all_blast.sh <blast db name> <fasta path>
+        all_vs_all_blast.sh <blast db name> <fasta path> <number of single jobs to run>
 
     Where:
         fasta path: file from which db was created
@@ -15,30 +16,44 @@ if [ $# -eq 0 ]
     exit 1
 fi
 
-mkdir temp
-mkdir out
-mkdir log
-echo -n > ./temp/neg_seqids
+if [ ! -d ./temp ]; then
+    mkdir temp
+fi
+rm temp/*
 
-pos=0
+if [ ! -d ./out ]; then
+    mkdir out
+fi
 
-while true; do
-    pos=`./single_fasta_acc.py ${FASTA} ${pos}`
-    currid=`grep '^>' ./temp/curr.fasta`
-    currid="${currid//>}"
+if [ ! -d ./log ]; then
+    mkdir log
+fi
+
+./partition_fasta.py ${FASTA} `grep -c "^>" ${FASTA}` ${N_JOBS}
+
+for i in $(seq ${N_JOBS});
+do
     # submit blastp job
-    if [ "$pos" -eq 0 ]; then
-        break
-    fi
-    cmd="blastp -outfmt 6 -query ./temp/curr.fasta -db ${DB} -negative_seqidlist ./temp/neg_seqids -out ./out/${currid} -num_threads=24"
+    cmd="blastp -outfmt 6 -query ./temp/partition_${i}.fasta -db ${DB} -out ./out/${i} -num_threads=24"
     sed -i "s@verbose.*@verbose ${cmd}@" ./submit_blast.sh
-    sed -i "s@job-name=.*@job-name=${currid}_blast@" ./submit_blast.sh
-    sed -i "s@log/.*@log/${currid}@" ./submit_blast.sh
-    while [ `squeue -u nmachnik | wc -l` -ge 100 ]; do
-        sleep 10
-    done
+    sed -i "s@job-name=.*@job-name=${i}_blast@" ./submit_blast.sh
+    sed -i "s@log/.*@log/${i}@" ./submit_blast.sh
     sbatch submit_blast.sh
-    # time blastp -outfmt 6 -query ./temp/curr.fasta -db ${DB} -negative_seqidlist ./temp/neg_seqids
 done
 
+# while true; do
+    
 
+#     if [ "$pos" -eq 0 ]; then
+#         break
+#     fi
+#     cmd="blastp -outfmt 6 -query ./temp/curr.fasta -db ${DB} -negative_seqidlist ./temp/neg_seqids -out ./out/${currid} -num_threads=24"
+#     sed -i "s@verbose.*@verbose ${cmd}@" ./submit_blast.sh
+#     sed -i "s@job-name=.*@job-name=${currid}_blast@" ./submit_blast.sh
+#     sed -i "s@log/.*@log/${currid}@" ./submit_blast.sh
+#     while [ `squeue -u nmachnik | wc -l` -ge 100 ]; do
+#         sleep 10
+#     done
+#     sbatch submit_blast.sh
+#     # time blastp -outfmt 6 -query ./temp/curr.fasta -db ${DB} -negative_seqidlist ./temp/neg_seqids
+# done
