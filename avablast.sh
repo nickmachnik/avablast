65e82945da573a0e6a1e98450697bde92828d1a8
Nick Machnik <nick.machnik@gmail.com>
2020-05-18 13:58:26 +0200

    make job script for each chunk

--- avablast.sh
+++ avablast.sh
@@ -18,33 +18,37 @@ if [ $# -eq 0 ]
     exit 1
 fi
 
+# get the scripts location
+BASELOC="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+exit 1
+
 if [ ! -d "${OUT_DIR}" ]; then
     mkdir "${OUT_DIR}"
 fi
 
-if [ ! -d "${OUT_DIR}"/results ]; then
-    mkdir "${OUT_DIR}"/results
-fi
-rm "${OUT_DIR}"/results/*
+# make or empty subdirectories for the output directory
+for SUBDIR in results temp log jobs;
+do
+    if [ ! -d "${OUT_DIR}"/"$SUBDIR" ]; then
+        mkdir "${OUT_DIR}"/"$SUBDIR"
+    fi
+    rm "${OUT_DIR}"/"$SUBDIR"/*
+done
 
-if [ ! -d "${OUT_DIR}"/temp ]; then
-    mkdir "${OUT_DIR}"/temp
-fi
-rm "${OUT_DIR}"/temp/*
-
-if [ ! -d "${OUT_DIR}"/log ]; then
-    mkdir "${OUT_DIR}"/log
-fi
-rm "${OUT_DIR}"/log/*
-
+# partition the input fasta in equally sized chunks
 ./partition_fasta.py "${FASTA}" "${OUT_DIR}"/temp "$(grep -c "^>" "${FASTA}")" "${N_JOBS}"
 
 for i in $(seq "${N_JOBS}");
 do
+    JOB_SCRIPT="$OUT_DIR"/jobs/submit_"$i".sh
+    # make sbatch script copy for this job
+    cp "$BASELOC"/submit_bash.sh "$JOB_SCRIPT"
+    
     # submit blastp job
     cmd="blastp -outfmt 6 -query ${OUT_DIR}/temp/partition_${i}.fasta -db ${DB} -out ${OUT_DIR}/results/${i} -num_threads=/${NUM_THREADS}"
-    sed -i "s@verbose.*@verbose ${cmd}@" ./submit_blast.sh
-    sed -i "s@job-name=.*@job-name=${i}_blast@" ./submit_blast.sh
-    sed -i "s@#SBATCH --output=.*@#SBATCH --output=${OUT_DIR}/log/${i}@" ./submit_blast.sh
-    sbatch submit_blast.sh
+    sed -i "s@verbose.*@verbose ${cmd}@" "$JOB_SCRIPT"
+    sed -i "s@job-name=.*@job-name=${i}_blast@" "$JOB_SCRIPT"
+    sed -i "s@#SBATCH --output=.*@#SBATCH --output=${OUT_DIR}/log/${i}@" "$JOB_SCRIPT"
+    sbatch "$JOB_SCRIPT"
 done
