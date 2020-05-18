8b00a6442edcb30fb9b13c39941ea18cd6ec1d86
Nick Machnik <nick.machnik@gmail.com>
2020-05-18 15:43:14 +0200

    Load latest blast and python modules

--- submit_blast.sh
+++ submit_blast.sh
@@ -38,8 +38,8 @@ unset SLURM_EXPORT_ENV
 #
 #
 #load the respective software module you intend to use
-#module load python/3.6.6
-module load ncbi-blast/2.9.0+
+module load python/3.8.2
+module load ncbi-blast/2.10.0+
 #
 #
 #run the respective binary through SLURM's srun
